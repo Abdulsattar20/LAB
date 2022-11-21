7ac08024b1efda4501a52c7f10a6eae7647bf6e7
Abdulsatttar20 <a.sattar.balushi.20@gmail.com>
2022-11-20 21:53:49 -0600

    main

--- account.py
+++ account.py
@@ -1,21 +1,22 @@
 class Account:
     def __init__(self, name:str):
         """
-        this function gets the holder account name and a defult amount of 0
-
+        this function create an account starting with name of account holder
+        :param name: persons first name
         """
-        self.account_name = name
-        self.account_balance = 0
+        self.__account_name = name
+        self.__account_balance = 0
 
 
 
     def deposit(self, amount) -> bool:
         """
-        this function claculate the deposit amount to the total balance
+        this function recive the amount that wanted to be deposited
+        :parm amount: amount of money that wanted to be deposited
         :return: if deposit succesful return true otherwise return false
         """
         if amount > 0 :
-            self.account_balance += amount
+            self.__account_balance += amount
             return True
         else:
             return False
@@ -24,11 +25,12 @@ class Account:
 
     def withdraw(self, amount:float)-> bool:
         """
-        this function claculate the withdraw amount from the total balance
+        this function recive the amount that wanted to be withdrown from it
+        :parm amount: amount of money that wanted to be withdrown
         :return: if transaction succesful return true otherwise return false
         """
-        if amount>0 and self.account_balance>0:
-            self.account_balance -= amount
+        if amount>0 and amount<= self.__account_balance:
+            self.__account_balance -= amount
             return True
         else:
             return False
@@ -40,7 +42,7 @@ class Account:
         this function get the balance
         :return: toatal balance
         """
-        return self.account_balance
+        return self.__account_balance
 
 
 
@@ -51,7 +53,7 @@ class Account:
         :return: holder name
         """
 
-        return self.account_name
+        return self.__account_name
 
 
 
